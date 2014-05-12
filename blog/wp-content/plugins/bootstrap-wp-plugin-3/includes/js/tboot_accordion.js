4275de9545f02578a4fc401dfd632ddcb2ad8ca9
Paul Cutler <paul.r.cutler@gmail.com>
2014-05-12 07:34:50 -0500

    Update theme and plugins

--- blog/wp-content/plugins/bootstrap-wp-plugin-3/includes/js/tboot_accordion.js
+++
@@ -1,1 +0,0 @@
-jQuery(function($){$(document).ready(function(){$(".tboot-accordion").accordion({autoHeight: false});});});
--- blog/wp-content/plugins/bootstrap-wp-plugin-3/includes/js/tboot_toggle.js
+++
@@ -1,1 +0,0 @@
-jQuery(function($){$(document).ready(function(){$("h3.tboot-toggle-trigger").click(function(){$(this).toggleClass("active").next().slideToggle("fast");return false;});});});
