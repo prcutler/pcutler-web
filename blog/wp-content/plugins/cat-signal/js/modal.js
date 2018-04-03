05c623ab5b67999192b89986d5f2c5e57569b689
Paul Cutler <paul.r.cutler@gmail.com>
2018-04-03 18:39:36 -0500

    First pass at switching to Hugo from Wordpress

--- blog/wp-content/plugins/cat-signal/js/modal.js
+++
@@ -1,8 +0,0 @@
-window._idl = {};
-	_idl.variant = "modal";
-	(function() {
-	    var idl = document.createElement('script');
-	    idl.async = true;
-	    idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant);
-	    document.getElementsByTagName('body')[0].appendChild(idl);
-	})();
